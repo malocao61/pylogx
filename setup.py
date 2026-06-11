3ad3e6003b64d89541890c545366b173ef6e85a3
malocao61 <salomaoiogurte31@proton.me>
2026-06-11 21:57:47 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 
 setup(
-    name="pylogx",
+    name="pylogxo",
     version="1.0.3",
     description="Extended logging utilities with color, rotation, and JSON output",
     long_description=open("README.md", encoding="utf-8").read(),
